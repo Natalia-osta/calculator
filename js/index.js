ba998717c7480fbf76d513d0bcaf1923253f17e1
Natalia-osta <ostas.natali@gmail.com>
2019-09-09 09:57:53 +0000

    update calc

--- js/index.js
+++ js/index.js
@@ -1,32 +1,52 @@
-const nums = document.querySelectorAll('.calc .nums button');
-    const opers = document.querySelectorAll('.calc .opers button');
+    const nums = document.querySelector('.nums');
     const display = document.querySelector('.calc .display');
     const equal = document.querySelector('.calc .eq');
     const clear = document.querySelector('.calc .clear');
     const back = document.querySelector('.calc .back');
-
+    const swicher = document.querySelector('.swicher');
+    const changer = document.querySelector('.changer');
 
     let res = 0;
     let x = [];
     let result = 0;
 
-    nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed) );
+    nums.addEventListener('click', numPressed);
     function numPressed(ev) {
+        console.log(ev.target);
+        if(ev.target.nodeName !== "BUTTON"){
+            return;
+        }
         x.push(display.value += ev.target.innerText);
-        console.log(x)
         return;
         }
-
     equal.addEventListener('click', calcOpereting );
     function calcOpereting(ev) {
-       res = x.pop();
-       result = eval(res);
-       display.value = result;
+        res = x.pop();
+        result = eval(res);
+        display.value = result;
     }
     clear.addEventListener('click', clearPressed );
     function clearPressed(ev) {
         result = 0;
         display.value = " ";
-    console.log(result)
     }
-
+    back.addEventListener('click', backSpace);
+    function backSpace(ev) {
+        const str = display.value;
+        display.value = str.substring(0, str.length - 1);
+    }
+    changer.addEventListener('click', changeValue);
+    function changeValue(ev) {
+        const str = display.value;
+        if(+str > 0){
+            display.value = `-${str}`;
+        }
+        else display.value = str.slice(1)
+    }
+    swicher.addEventListener('click', toogleOff);
+    function toogleOff(ev) {
+        display.classList.toggle("hide")
+        swicher.classList.toggle("online")
+        result = 0;
+        display.value = " ";
+    }
